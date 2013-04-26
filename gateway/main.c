4585bd9e3979ba777276e015e94b57278b7f8d77
kehribar <ihsan@kehribar.me>
2013-04-26 23:26:55 +0300

    add notice on gateway example

--- gateway/main.c
+++ gateway/main.c
@@ -5,7 +5,7 @@
 * can do whatever you want with this stuff. If we meet some day, and you think
 * this stuff is worth it, you can buy me a coffee in return.
 * -----------------------------------------------------------------------------
-* This example is work in progres ... 
+* This example is work in progress ... 
 * -----------------------------------------------------------------------------
 */
 #include <avr/io.h>
